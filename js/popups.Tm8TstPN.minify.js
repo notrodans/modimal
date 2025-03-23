fd94a3181a13712fab1718c015f4bdf46115f117
Dmytro Zhukov <notrodans@gmail.com>
2025-03-23 22:47:39 +0200

    Обновления сборки, stylelint, модалки корзины и другие улучшения

---
+++ js/popups.Tm8TstPN.minify.js
@@ -0,0 +1,1 @@
+import{P as p}from"./app.SoVsoFlL.minify.js";new p("cart-popup-primary",{classes:{popup:"cart-popup",wrapper:"cart-popup__wrapper",container:"cart-popup__container",content:"cart-popup__content"}});
