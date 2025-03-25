3fd721c2b12ea5bd502c4e261bfee7a2874c5fe5
Dmytro Zhukov <notrodans@gmail.com>
2025-03-25 07:22:28 +0200

    Страница корзины

---
+++ js/popups.DPy8I8Up.minify.js
@@ -0,0 +1,1 @@
+import{P as p}from"./app.EYjH0Dxm.minify.js";new p("cart-popup-primary",{classes:{popup:"cart-popup",wrapper:"cart-popup__wrapper",container:"cart-popup__container",content:"cart-popup__content"}});
